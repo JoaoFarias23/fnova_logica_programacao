10fecef4c601199d05edcb27dad28fa4427b392b
João Lucas <127903901+JoaoFarias23@users.noreply.github.com>
2023-03-28 12:21:19 -0300

    Soma
    Atividade de soma.

---
+++ Soma.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Soma {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("Coloque um numero \n");
+
+        double calculoX = scanner.nextDouble();
+
+        System.out.printf("Coloque um numero \n");
+
+        double calculoY = scanner.nextDouble();
+
+        double soma = calculoX + calculoY;
+        System.out.println("Soma de calculo 1 com calculo 2 = " + soma);
+    }
+}
+
+    
+
