6e4e9fa146b55e489b68cdbbebdf78a8227557c2
João Lucas <127903901+JoaoFarias23@users.noreply.github.com>
2023-03-29 01:31:35 -0300

    Update SextaQuestao.java

--- SextaQuestao.java
+++ SextaQuestao.java
@@ -5,31 +5,30 @@ public class SextaQuestao {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Sera parcelado ou à vista ?");
         String tipoPagamento = scanner.nextLine();
+        String pagamento = "";
 
-        String pagamento;
-
-        if(tipoPagamento.contains("parcelado")){
-            pagamento = "credito";
-        }else{
-            System.out.println("Pix ou debito?");
+        if (!tipoPagamento.contains("parcelado")) {
+            System.out.println("pix,credito ou debito?");
             pagamento = scanner.nextLine();
+
         }
+
 
         double preco = 1500.00;
 
-        if (tipoPagamento.contains("parcelado") && pagamento.contains("credito") ) {
+        if (tipoPagamento.contains("parcelado")) {
             System.out.println("Digite a quantidade de parcelas");
-            Integer quantidadeParcelas = scanner.nextInt();
-            Double precoParcelado = (preco / quantidadeParcelas) * 0.10;
-            Double precoFinal = preco / quantidadeParcelas + precoParcelado;
+            int quantidadeParcelas = scanner.nextInt();
+            double precoParcelado = (preco / quantidadeParcelas) * 0.10;
+            double precoFinal = preco / quantidadeParcelas + precoParcelado;
             System.out.println("Preco total " + preco + ", e o preco de cada parcela com uma taxa de 10% ao mes " + precoFinal);
-        } else if (pagamento.contains("debito")) {
+        } else if (tipoPagamento.contains("vista") && pagamento.contains("credito")) {
 
-            System.out.println("Preco a vista no debito R$"+ preco);
+            System.out.println("Preco a vista no credito a vista R$" + preco);
 
-        }else{
-            Double precoPix = preco - (preco * 0.10);
-            System.out.println("Preco a vista no pix R$"+precoPix);
+        } else {
+            double precoPix = preco - (preco * 0.10);
+            System.out.println("Preco a vista no pix/debito R$" + precoPix);
         }scanner.close();
 
     }
